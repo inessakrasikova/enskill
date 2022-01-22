b1e3a2ff900c2702bc834ae51c3f424a71c508ff
aruytehno <aruytehno@gmail.com>
2022-01-22 15:58:42 +0300

    Update project

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,84 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+var inputMassive;
+var outputMassive;
+
+var numberOfMistakes = 0;
+var wordNumber = 0;
+
+function selectTheme() {
+    var navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach((navLink) => {
+        navLink.addEventListener("click", function (e) {
+            var theme = navLink.dataset.theme;
+
+            /**
+             * Подгрузка JSON из файла
+             */
+            fetch("./json/words.json")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((words) => {
+                    if (words[theme] !== undefined && words[theme].length > 0) {
+                        console.log(`words[${theme}]: `, words[theme]);
+                        inputMassive = words[theme][0]["ru"];
+                        outputMassive = words[theme][1]["en"];
+
+                        console.log("inputMassive: ", inputMassive);
+                        console.log("outputMassive: ", outputMassive);
+                    } else {
+                        console.log(`words[${theme}] не существует`);
+                    }
+                })
+                .then(() => {
+                    numberOfMistakes = 0;
+                    wordNumber = 0;
+                    showWord();
+                })
+                .catch((err) => {
+                    // Do something for an error here
+                    console.log(err);
+                });
+        });
+    });
+}
+
+function showWord() {
+    document.getElementById("word").innerHTML = inputMassive[wordNumber];
+}
+
+function checkWord() {
+    var translate = document.getElementById("translate").value;
+
+    if (translate == outputMassive[wordNumber]) {
+        document.getElementById("translate").value = "";
+        document.getElementById("translate").placeholder = "Write your translation";
+        numberOfMistakes = 0;
+        wordNumber++;
+    } else {
+        document.getElementById("translate").value = "";
+        numberOfMistakes++;
+    }
+
+    if (numberOfMistakes == 3) {
+        document.getElementById("translate").placeholder = outputMassive[wordNumber];
+        numberOfMistakes = 0;
+    }
+
+    if (wordNumber == outputMassive.length) {
+        wordNumber = 0;
+    }
+    showWord();
+}
+
+var input = document.getElementById("translate");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        checkWord();
+    }
+});
