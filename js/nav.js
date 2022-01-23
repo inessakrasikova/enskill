032a255b5a207e2a4be3830f283f6fd77117c5dc
aruytehno <aruytehno@gmail.com>
2022-01-23 06:12:53 +0300

    Update

--- js/nav.js
+++ js/nav.js
@@ -2,14 +2,26 @@ var nav = document.getElementById("navigation");
 /**
  * Формирование ссылок на пункты меню
  */
-function linkHref(link, name) {
-  return `<a class="dropdown-item" href="#" data-theme="${link}">${name}</a>`;
+function linkHref1(link, name) {
+  return `<a class="dropdown-item" href="#">${name}</a>`;
 }
 
+function linkHref2(link, name) {
+  return `<a class="dropdown-item dropdown-toggle" href="#">${name}</a>`;
+}
+function linkHref(link, name) {
+  return `<a href="#" class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-theme="${link}">${name}</a>`;
+}
+
+/**
+ * Создаёт элементы топика
+ * @param parent
+ * @param childdata
+ */
 function subsubmenu(parent, childdata) {
   var submenumainlink = document.getElementById("submenu-" + parent.keyName);
   var submenu = document.createElement("ul");
-  submenu.className = "dropdown-menu dropdown-submenu shadow";
+  submenu.className = "dropdown-menu shadow";
   submenu.setAttribute("id", "submenu-" + parent.keyName + "-menu");
   submenumainlink.appendChild(submenu);
 
@@ -30,6 +42,11 @@ function subsubmenu(parent, childdata) {
   }
 }
 
+/**
+ * Создаёт список топиков по теме
+ * @param parent
+ * @param childdata
+ */
 function submenu(parent, childdata) {
   var submenumainlink = document.getElementById("submenu-" + parent.keyName);
   var submenu = document.createElement("ul");
@@ -42,30 +59,35 @@ function submenu(parent, childdata) {
 
   for (item of childdata) {
     var line = document.createElement("li");
-    line.className = item.submenu ? "nav-item submenu" : "nav-item";
-    line.innerHTML = linkHref(item.link, item.title);
-
     if (item.submenu) {
+
+      line.className = item.submenu ? "nav-item submenu" : "nav-item";
+      line.innerHTML = linkHref(item.link, item.title);
       submenuId.appendChild(line);
       line.setAttribute("id", "submenu-" + item.keyName);
       subsubmenu(item, item.submenu);
     } else {
+      line.innerHTML = linkHref2(item.link, item.title);
       submenuId.appendChild(line);
     }
   }
 }
 
+/**
+ * Создаёт список тем
+ * @param data
+ */
 function createLink(data) {
   for (navLink of data) {
     var line = document.createElement("li");
-    line.className = navLink.submenu ? "nav-item submenu" : "nav-item";
-    line.innerHTML = linkHref(navLink.link, navLink.title);
     if (navLink.submenu) {
-      //submenu(navLink, navLink.submenu);
+      line.className = navLink.submenu ? "dropstart" : "nav-item";
+      line.innerHTML = linkHref(navLink.link, navLink.title);
       line.setAttribute("id", "submenu-" + navLink.keyName);
       nav.appendChild(line);
       submenu(navLink, navLink.submenu);
     } else {
+      line.innerHTML = linkHref1(navLink.link, navLink.title);
       nav.appendChild(line);
     }
   }
